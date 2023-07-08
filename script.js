ff042c6b94bd12e629324631ec93989d9326ab33
Yekaterina Pavlova <cait.pavlova@inbox.ru>
2023-07-08 18:08:29 +0300

    flat and flatMap

--- script.js
+++ script.js
@@ -331,3 +331,25 @@ const isDeposit = mov => mov > 0;
 console.log(movements.some(isDeposit));
 console.log(movements.every(isDeposit));
 console.log(movements.filter(isDeposit));
+
+// --------- flat and flatMap ES2019
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat()); // [ 1, 2, 3, 4, 5, 6, 7, 8 ]
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat()); //  [ [1, 2], 3, 4, [5, 6], 7, 8 ]
+// parameter = 1 - deep level:
+console.log(arrDeep.flat(2)); // [ 1, 2, 3, 4, 5, 6, 7, 8 ]
+
+//
+const overallBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance);
+
+// flatMap - compines a map and a flat methods, better for performance
+const overallBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance2);
